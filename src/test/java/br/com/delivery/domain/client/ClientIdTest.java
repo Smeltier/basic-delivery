4122f09fcef751d8cd13ea745eff8653d7bd4d69
Gabriel G. <connect.goncalves@gmail.com>
2026-02-13 19:26:43 -0300

    add: adiciona teste para ClientId.

--- src/test/java/br/com/delivery/domain/client/ClientIdTest.java
+++ src/test/java/br/com/delivery/domain/client/ClientIdTest.java
@@ -13,4 +13,35 @@ public class ClientIdTest {
 
     assertEquals(uuid, id.value());
   }
+
+  @Test
+  void shouldThrowExceptionWhenUUIDIsNull() {
+    assertThrows(NullPointerException.class,
+        () -> new ClientId(null));
+  }
+
+  @Test
+  void generateShouldReturnNonNullValue() {
+    ClientId id = ClientId.generate();
+
+    assertNotNull(id);
+    assertNotNull(id.value());
+  }
+
+  @Test
+  void generateShouldCreateDifferentIds() {
+    ClientId id1 = ClientId.generate();
+    ClientId id2 = ClientId.generate();
+
+    assertNotEquals(id1, id2);
+  }
+
+  @Test
+  void twoClientsWithSameUUIDNeedToBeEquals() {
+    UUID id = UUID.randomUUID();
+    ClientId id1 = new ClientId(id);
+    ClientId id2 = new ClientId(id);
+
+    assertEquals(id1, id2);
+  }
 }
