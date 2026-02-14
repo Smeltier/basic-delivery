f4f05d3e9b4a438406415b4764231df6e7aae70a
Gabriel G. <connect.goncalves@gmail.com>
2026-02-13 22:55:16 -0300

    add: adiciona testes para a classe Money.

--- src/test/java/br/com/delivery/domain/shared/EmailTest.java
+++ src/test/java/br/com/delivery/domain/shared/EmailTest.java
@@ -5,8 +5,39 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class EmailTest {
   @Test
-  void shouldNotBeAbleToCreateWithBlankValid() {
+  void shouldCreateValidEmail() {
+    var email = new Email("user@test.com");
+
+    assertEquals("user@test.com", email.value());
+  }
+
+  @Test
+  void shouldConvertEmailToLowerCase() {
+    var str = "USER@TEST.COM";
+    var email = new Email(str);
+
+    assertEquals(str.toLowerCase(), email.value());
+  }
+
+  @Test
+  void shouldNotBeAbleToCreateWithBlankValue() {
     assertThrows(IllegalArgumentException.class,
-        () -> new Email(""));
+        () -> new Email("connect.gocalves@gmail.com"));
+  }
+
+  @Test
+  void shouldNotBeAbleToCreateWithInvalidMatch() {
+    assertThrows(IllegalArgumentException.class,
+        () -> new Email("///@gmail.com"));
+  }
+
+  @Test
+  void emailsWithSameValueShouldBeEqual() {
+    var str = "user@test.com";
+
+    var em1 = new Email(str);
+    var em2 = new Email(str);
+
+    assertEquals(em1, em2);
   }
 }
