323535e96d60fa00507eb3b8bd3c4f1687cd061a
Smeltier <connect.goncalves@gmail.com>
2026-02-15 19:03:51 -0300

    att: move pasta src/ para backend/

---
+++ backend/src/test/java/br/com/delivery/domain/payment/PaymentTest.java
@@ -0,0 +1,12 @@
+// package br.com.delivery.domain.payment;
+//
+// import org.junit.jupiter.api.Test;
+// import static org.junit.jupiter.api.Assertions.*;
+//
+// public class PaymentTest {
+// @Test
+// void shouldCreateWithPendingState() {
+// var payment = new Payment(PaymentId.generate());
+// assertTrue(())
+// }
+// }
