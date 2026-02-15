a566cecd2a4dc0ea103a7c360ecae60a39dae613
Smeltier <connect.goncalves@gmail.com>
2026-02-15 18:29:00 -0300

    att: atualiza retorno do método process da classe PaymentMethod para PaymentProcessingResult.

--- src/main/java/br/com/delivery/domain/payment/PaymentMethod.java
+++ src/main/java/br/com/delivery/domain/payment/PaymentMethod.java
@@ -3,5 +3,5 @@ package br.com.delivery.domain.payment;
 import br.com.delivery.domain.shared.Money;
 
 public interface PaymentMethod {
-  void process(Money amount);
+  PaymentProcessingResult process(Payment payment);
 }
---
+++ src/main/java/br/com/delivery/domain/payment/PaymentProcessingResult.java
@@ -0,0 +1,7 @@
+package br.com.delivery.domain.payment;
+
+public enum PaymentProcessingResult {
+  APPROVED,
+  REJECTED,
+  PENDING
+}
