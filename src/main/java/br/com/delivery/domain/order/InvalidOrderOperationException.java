e782243efe08bf90201441892d0b41a8f4afc299
Smeltier <connect.goncalves@gmail.com>
2026-02-15 18:59:48 -0300

    att: remove InvalidOrderOperationException de domain/order.

--- src/main/java/br/com/delivery/domain/order/InvalidOrderOperationException.java
+++
@@ -1,7 +0,0 @@
-package br.com.delivery.domain.order;
-
-public class InvalidOrderOperationException extends RuntimeException {
-  public InvalidOrderOperationException(String message) {
-    super(message);
-  }
-}
